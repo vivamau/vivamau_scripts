428832f7444088b1e4a00e4f28fd31510401e7d8
vivamau <maurizio.blasilli@gmail.com>
2022-01-23 22:17:15 +0300

    first script to extract users from a yammer community

---
+++ yammer_UsersInACommunity.py
@@ -0,0 +1,25 @@
+import requests
+# go here to create a token for your app
+# https://www.yammer.com/client_applications
+token = "THISISWHEREMYTOKENGOES"
+
+# this is the community ID
+groupID = "78686445568"
+
+page = 1
+pagesize = 50
+endpoint = "https://www.yammer.com/api/v1/users/in_group/{}.json?page={}".format(groupID,page)
+headers = {"Authorization": "Bearer " + token}
+
+data = requests.get(endpoint, headers=headers).json()
+
+numberofpages = data['total_count']/pagesize
+
+for x in range(numberofpages+1):
+	# print("THIS IS PAGE {}".format(page))
+	page = page + 1
+	for i in data['users']:
+		print(i["email"] + ";")
+	endpoint = "https://www.yammer.com/api/v1/users/in_group/{}.json?page={}".format(groupID,page)
+	data = requests.get(endpoint, headers=headers).json()
+
