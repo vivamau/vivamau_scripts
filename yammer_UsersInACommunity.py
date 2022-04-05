c0595b1551769fb9ef10285f29ee65cc03c28e40
vivamau <maurizio.blasilli@gmail.com>
2022-04-05 22:20:39 +0300

    fixed an issue with floating numbers for numberofpages, added counting of users

--- yammer_UsersInACommunity.py
+++ yammer_UsersInACommunity.py
@@ -1,7 +1,7 @@
 import requests
 # go here to create a token for your app
 # https://www.yammer.com/client_applications
-token = "THISISWHEREMYTOKENGOES"
+token = "MYTOKENGOESHERE!"
 
 # this is the community ID
 groupID = "78686445568"
@@ -13,13 +13,15 @@ headers = {"Authorization": "Bearer " + token}
 
 data = requests.get(endpoint, headers=headers).json()
 
-numberofpages = data['total_count']/pagesize
-
+numberofpages = data['total_count']//pagesize
+count = 0
 for x in range(numberofpages+1):
 	# print("THIS IS PAGE {}".format(page))
 	page = page + 1
+
 	for i in data['users']:
+		count = count + 1
 		print(i["email"] + ";")
 	endpoint = "https://www.yammer.com/api/v1/users/in_group/{}.json?page={}".format(groupID,page)
 	data = requests.get(endpoint, headers=headers).json()
-
+print ("number of users: {}".format(count))
